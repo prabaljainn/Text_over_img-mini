390031cb59ac2f35a6bf1348aea71c754757dd96
Prabal Jain <prabaljainn@gmail.com>
2021-12-16 16:07:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ myFont = ImageFont.truetype('fonts/Ubuntu.ttf', size=150)
 d1 = ImageDraw.Draw(image)
 
 
-d1.text((400, 100), "Success", fill=(5, 0, 50), font=myFont)
+d1.text((450, 100), "Success", fill=(5, 0, 50), font=myFont)
 
 
 messege = " is a journey not a Destination."
 
 
-d1.text((450, 200), messege, fill=(5, 0, 50), font=myFont)
+d1.text((450, 300), messege, fill=(5, 0, 50), font=myFont)
 
 
 image.show()
